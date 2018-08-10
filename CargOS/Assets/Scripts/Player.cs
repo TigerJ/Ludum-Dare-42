6e8bf9a9d483b6d92077acef8b1d71de9eee44f5
TigerJ <jbriggs@leafsoftwaresolutions.com>
2018-08-10 19:36:30 -0400

    CargOS first commit look mom I am making a game!

---
+++ CargOS/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Left")) movePlayer("Left");
+        if (Input.GetButtonDown("Right")) movePlayer("Right");
+        if (Input.GetButtonDown("Down")) movePlayer("Down");
+        if (Input.GetButtonDown("Up")) movePlayer("Up");
+    }
+    void movePlayer(string Direction)
+    {
+        if (Direction == "Up")
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+        }
+        if (Direction == "Down")
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z);
+        } 
+        if (Direction == "Right")
+        {
+            transform.position = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+        }
+        if (Direction == "Left")
+        {
+            transform.position = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
+        }
+    }
+}
