0f216369952feb297a04b3f89c04db7caa3ffd34
TigerJ <jbriggs@leafsoftwaresolutions.com>
2018-08-12 18:08:59 -0400

    updated after final build with new gitignore to protect dev of custom font from itch.io

---
+++ CargOS/Assets/Scripts/end.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class end : MonoBehaviour {
+    public Text finalScore;
+	// Use this for initialization
+	void Start () {
+		
+	}
+    private void Awake()
+    {
+         finalScore.text = "your score was: " + GameObject.Find("ScoreHolder").GetComponent<LastScore>().score.ToString("D7");
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
